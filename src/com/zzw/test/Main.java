f0504b95fdef3337083b98f775957c41e844b00f
zhangzhiwei <3053239351@qq.com>
2021-09-14 13:56:30 +0800

    Initiate

--- src/com/zzw/test/Main.java
+++ src/com/zzw/test/Main.java
@@ -3,15 +3,7 @@ package com.zzw.test;
 public class Main {
 
     public static void main(String[] args) {
-        int[] nums = {1, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 5, 5, 5, 5, 5, 7, 10, 10, 14};
-        int count = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
-                continue;
-            }
-            count += 1;
-        }
-        System.out.println(count + 1);
+        System.out.println("Hello LeetCode!");
     }
 
 }
